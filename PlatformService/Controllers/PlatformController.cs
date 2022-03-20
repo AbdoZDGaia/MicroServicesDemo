6f46515dd4621c75cefa01273b5a995b34df4ae0
AbdoZDGaia <oldtimeprogrammer@yahoo.com>
2022-03-20 12:05:56 +0200

    Platform controller added

---
+++ PlatformService/Controllers/PlatformController.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using PlatformService.Data;
+using PlatformService.Dtos;
+
+namespace PlatformService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlatformController : ControllerBase
+    {
+        private readonly IPlatformRepo _repo;
+        private readonly IMapper _mapper;
+
+        public PlatformController(IPlatformRepo repo, IMapper mapper)
+        {
+            _repo = repo;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public IActionResult GetPlatforms()
+        {
+            Console.WriteLine("--> Getting platforms...");
+            var platformItems = _repo.GetAllPlatforms();
+            var results = _mapper.Map<IEnumerable<PlatformReadDto>>(platformItems);
+            return Ok(results);
+        }
+    }
+}
