d99ca8bc5562f7b0f655e78bdf14acb3b0a76b0a
AbdoZDGaia <oldtimeprogrammer@yahoo.com>
2022-03-20 02:22:03 +0200

    Add project files.

---
+++ PlatformService/Data/PlatformRepo.cs
@@ -0,0 +1,39 @@
+﻿using PlatformService.Models;
+
+namespace PlatformService.Data
+{
+    public class PlatformRepo : IPlatformRepo
+    {
+        private readonly AppDbContext _ctx;
+
+        public PlatformRepo(AppDbContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+        public void CreatePlatform(Platform platform)
+        {
+            if(platform == null)
+            {
+                throw new ArgumentNullException(nameof(platform));
+            }
+
+            _ctx.Platforms.Add(platform);
+        }
+
+        public IEnumerable<Platform> GetAllPlatforms()
+        {
+            return _ctx.Platforms.ToList();
+        }
+
+        public Platform GetPlatformById(int id)
+        {
+            return _ctx.Platforms.FirstOrDefault(p => p.Id == id);
+        }
+
+        public bool SaveChanges()
+        {
+            return (_ctx.SaveChanges() >= 0);
+        }
+    }
+}
---
+++ PlatformService/Models/Platform.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PlatformService.Models
+{
+    public class Platform
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Publisher { get; set; }
+        [Required]
+        public string Cost { get; set; }
+    }
+}
