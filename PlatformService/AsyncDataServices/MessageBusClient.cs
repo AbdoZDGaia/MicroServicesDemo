8d6a7e581f7d36b086b39f0b8cf0bc646096663e
AbdoZDGaia <oldtimeprogrammer@yahoo.com>
2022-03-30 21:49:23 +0200

    RabbitMQ implemented in PlatformService

---
+++ PlatformService/AsyncDataServices/MessageBusClient.cs
@@ -0,0 +1,44 @@
+﻿using PlatformService.Dtos;
+using RabbitMQ.Client;
+
+namespace PlatformService.AsyncDataServices
+{
+    public class MessageBusClient : IMessageBusClient
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IConnection _connection;
+        private readonly IModel _channel;
+
+        public MessageBusClient(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            var factory = new ConnectionFactory()
+            {
+                HostName = _configuration["RabbitMQ:Host"],
+                Port = int.Parse(_configuration["RabbitMQ:Port"]),
+            };
+            try
+            {
+                _connection = factory.CreateConnection();
+                _channel = _connection.CreateModel();
+                _channel.ExchangeDeclare(exchange: _configuration["RabbitMQ:Exchange"], type: ExchangeType.Fanout);
+                _connection.ConnectionShutdown += RabbitMQ_ConnectionShutdown;
+                Console.WriteLine("--> RabbitMQ connection established");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("--> Could not connect to Message Bus: " + ex.Message);
+            }
+        }
+
+        private void RabbitMQ_ConnectionShutdown(object sender, ShutdownEventArgs e)
+        {
+            Console.WriteLine("--> RabbitMQ Connection Shutdown");
+        }
+
+        public void PublishNewPlatform(PlatformPublishDto platformPublishDto)
+        {
+            
+        }
+    }
+}
